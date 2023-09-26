605ddd7c299a239010be1b7d6674427219eb70ec
Noahseltene <fevenseltene23@gmail.com>
2023-09-26 18:40:01 +0300

    add sound

--- oop.py
+++ oop.py
@@ -4,16 +4,18 @@ class Dog:
     attr1 = "mammal"
  
     # Instance attribute
-    def __init__(self, name, color):
+    def __init__(self, name, color, sound):
         self.name = name
         self.color = color
+        self.sound = sound
+
+    def speak(self):  
+        print("This dog {}".format(self.sound))
  
-# Driver code
+
 # Object instantiation
-
-
-pesh = Dog("Pesh", "green")
-Tee = Dog("Tee", "blue")
+pesh = Dog("Pesh", "green", "barks")
+Tee = Dog("Tee", "blue", "barks")
  
 # Accessing class attributes
 print("Pesh is a {}".format(pesh.attr1))
@@ -27,3 +29,12 @@ print("My name is {}".format(Tee.name))
 # Accessing instance attributes
 print("My color is {}".format(pesh.color))
 print("My color is {}".format(Tee.color))
+
+
+# Accessing class methods
+pesh.speak()
+Tee.speak()
+ 
+# Accessing instance attributes
+print("My name is {}".format(pesh.name))
+print("My name is {}".format(Tee.name))
